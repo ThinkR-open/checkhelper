9f22542f3ff610ee25a3befa7d14cda582aa8dfe
statnmap <statnmap@users.noreply.github.com>
2019-08-19 12:45:05 +0200

    try deploy on gh-pages

--- tic.R
+++ tic.R
@@ -6,3 +6,7 @@ do_package_checks()
 if (ci_on_travis()) {
   do_pkgdown()
 }
+
+get_stage("deploy") %>%
+  add_step(step_build_pkgdown()) %>%
+  add_step(step_push_deploy(branch = "gh-pages", path = "docs"))
