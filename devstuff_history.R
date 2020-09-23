661aee63f50ce2136d15952d7c1900d05ffb7b71
Sébastien Rochette <sebastienrochettefr@gmail.com>
2020-09-23 17:38:15 +0200

    new globals output

--- devstuff_history.R
+++ devstuff_history.R
@@ -47,6 +47,7 @@ travis::travis_set_pat()
 # usethis::use_appveyor()
 usethis::use_github_action_check_standard()
 usethis::use_github_action("pkgdown")
+usethis::use_github_action("test-coverage")
 
 # Development ----
 attachment::att_to_description() #dir.v = ""
