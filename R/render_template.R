50792e0cdcca081cc2fcc6556f07dc8cb7b0d38c
cervan <cervan@thinkr.fr>
2021-10-29 13:28:05 +0200

    Add use_data_doc to doument data into package easier close #14

---
+++ R/render_template.R
@@ -0,0 +1,13 @@
+#' Render template for data documentation
+#'
+#' This use double stash template.
+#'
+#' @param path path to the template.
+#' @param data list of information to replace in the template.
+#'
+#' @return use for this side effect. Make a file
+#' @noRd
+render_template <- function(path_template, path_to_save ,data){
+  render <- whisker::whisker.render(readLines(path_template, encoding = "UTF-8", warn = FALSE), data)
+  writeLines(render, con = path_to_save)
+}
