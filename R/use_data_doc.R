58b2ae068f5677e9d7ab264c8102c176c8cc7df5
Arthur Bréant <arthur@thinkr.fr>
2023-01-18 11:14:48 +0100

    chore: find missing tags
    What?
    
    * Run checkhelper::find_missing_tags
    
    Why?
    
    * Check for missing tags in checkhelper

--- R/use_data_doc.R
+++ R/use_data_doc.R
@@ -1,9 +1,11 @@
 #' Documentation of .rda in package
 #'
-#' @param name Name of your data. only the name, 
+#' @param name Name of your data. only the name,
 #' @param prefix Add prefix for the name of R script
 #' @param description Add a description
 #' @param source Add a source
+#'
+#' @return Create a data documentation and return path to the file
 #'
 #' @export
 #'
@@ -13,29 +15,28 @@
 #' \dontrun{
 #' use_data_doc("my_data", description = "Desc of my_data", source = "Here my source")
 #' }
-use_data_doc <- function(name, prefix = "doc_", description = "Description", source = "Source"){
-  if(!file.exists("DESCRIPTION") & dir.exists("R")){
+use_data_doc <- function(name, prefix = "doc_", description = "Description", source = "Source") {
+  if (!file.exists("DESCRIPTION") & dir.exists("R")) {
     stop("You have to be in package.")
   }
-  
+
   path <- glue("R/{prefix}{name}.R")
-  
+
   render_template(
-    path_template =  system.file("template", "data-doc.R", package = "checkhelper"),
+    path_template = system.file("template", "data-doc.R", package = "checkhelper"),
     path_to_save = path,
     data = get_data_info(name, description, source)
   )
-  
-  if(!requireNamespace("cli")){
+
+  if (!requireNamespace("cli")) {
     cli::cli_alert_success(
       glue("Adding the data documentation in {path}")
-      )
+    )
     invisible(path)
-  }else{
+  } else {
     message(
       glue("Adding the data documentation in {path}")
-      )
+    )
     invisible(path)
   }
-  
 }
