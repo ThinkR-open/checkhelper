a017587901e84706193240a71a0b7fd44607a779
Arthur Bréant <arthur@thinkr.fr>
2023-03-23 12:26:41 +0100

    (wip) feat: Manage false positive missing tags
    tags: feat
    
    Why?
    
    * Some tags are not missing
    
    What?
    
    * Mostly tags relative to documentation
    
    Issue #70

--- R/find_missing_tags.R
+++ R/find_missing_tags.R
@@ -6,6 +6,7 @@
 #' @importFrom utils getFromNamespace
 #' @importFrom dplyr mutate filter left_join if_else tibble
 #' @importFrom dplyr group_by summarise first select n
+#' @importFrom purrr walk map keep
 #' @export
 #'
 #' @examples
@@ -16,57 +17,114 @@
 #' # A reproducible example on a test package
 #' pkg_path <- create_example_pkg()
 #' find_missing_tags(pkg_path)
-find_missing_tags <- function (package.dir = ".", roclets = NULL, load_code = NULL,
-                                 clean = FALSE)
-{
-  # browser()
+find_missing_tags <- function(package.dir = ".",
+                              roclets = NULL,
+                              load_code = NULL,
+                              clean = FALSE) {
   base_path <- normalizePath(package.dir, mustWork = TRUE)
-  # is_first <- roxygen2:::roxygen_setup(base_path)
-  # is_first <- getFromNamespace("roxygen_setup", "roxygen2")(base_path)
+
   encoding <- desc::desc_get("Encoding", file = base_path)[[1]]
+
   if (!identical(encoding, "UTF-8")) {
     warning("roxygen2 requires Encoding: UTF-8", call. = FALSE)
   }
-  # roxygen2:::roxy_meta_load(base_path)
+
   getFromNamespace("roxy_meta_load", "roxygen2")(base_path)
+
   packages <- roxygen2::roxy_meta_get("packages")
+
   lapply(packages, loadNamespace)
-  # roclets <- roxygen2:::`%||%`(roclets, roxy_meta_get("roclets"))
+
   roclets <- getFromNamespace("%||%", "roxygen2")(roclets, roxygen2::roxy_meta_get("roclets"))
+
   if ("collate" %in% roclets) {
     roxygen2::update_collate(base_path)
     roclets <- setdiff(roclets, "collate")
   }
-  if (length(roclets) == 0)
-    return(invisible())
-  roclets <- lapply(roclets, roxygen2::roclet_find)
-  # load_code <- roxygen2:::find_load_strategy(load_code)
-  load_code <- getFromNamespace("find_load_strategy", "roxygen2")(load_code)
-  env <- load_code(base_path)
-  # roxygen2:::roxy_meta_set("env", env)
-  getFromNamespace("roxy_meta_set", "roxygen2")("env", env)
-  # on.exit(roxygen2:::roxy_meta_set("env", NULL), add = TRUE)
-  on.exit(getFromNamespace("roxy_meta_set", "roxygen2")("env", NULL), add = TRUE)
-  # blocks <- roxygen2:::parse_package(base_path, env = NULL)
-  blocks <- getFromNamespace("parse_package", "roxygen2")(base_path, env = NULL)
-  if (clean) {
-    # purrr::walk(roclets, roxygen2:::roclet_clean, base_path = base_path)
-    purrr::walk(roclets, getFromNamespace("roclet_clean", "roxygen2"), base_path = base_path)
-  }
-  # roclets <- lapply(roclets, roxygen2:::roclet_preprocess, blocks = blocks,
-  roclets <- lapply(roclets, getFromNamespace("roclet_preprocess", "roxygen2"), blocks = blocks,
-                    base_path = base_path)
-  # blocks <- lapply(blocks, roxygen2:::block_set_env, env = env)
-  blocks <- lapply(blocks, getFromNamespace("block_set_env", "roxygen2"), env = env)
 
-  res_topic <- lapply(blocks, function(x) {
+  if (length(roclets) == 0) {
+    return(invisible())
+  }
+
+  roclets <- lapply(roclets, roxygen2::roclet_find)
+
+  load_code <- getFromNamespace("find_load_strategy", "roxygen2")(load_code)
+
+  env <- load_code(base_path)
+
+  getFromNamespace("roxy_meta_set", "roxygen2")("env", env)
+
+  on.exit(getFromNamespace("roxy_meta_set", "roxygen2")("env", NULL),
+    add = TRUE
+  )
+
+  blocks <- getFromNamespace("parse_package", "roxygen2")(base_path, env = NULL)
+
+  if (clean) {
+    walk(
+      roclets,
+      getFromNamespace("roclet_clean", "roxygen2"),
+      base_path = base_path
+    )
+  }
+
+  roclets <- lapply(
+    roclets,
+    getFromNamespace("roclet_preprocess", "roxygen2"),
+    blocks = blocks,
+    base_path = base_path
+  )
+
+  blocks <- lapply(
+    blocks,
+    getFromNamespace("block_set_env", "roxygen2"),
+    env = env
+  )
+
+  ##
+  ## Split blocks into functions or documentations
+  ##
+  res_functions <- purrr::map(
+    .x = blocks,
+    .f = ~ if (class(.x[["object"]])[1] == "function") .x
+  ) %>%
+    purrr::compact()
+
+  res_package <- purrr::map(
+    .x = blocks,
+    .f = ~ if (class(.x[["object"]])[1] == "package") .x
+  ) %>%
+    purrr::compact()
+
+  res_data <- purrr::map(
+    .x = blocks,
+    .f = ~ if (class(.x[["object"]])[1] == "data") .x
+  ) %>%
+    purrr::compact()
+
+
+  ## Check for missing tags
+
+  ### Package documentation
+  ###
+  res_package_keywords <- purrr::map(res_package, roxygen2::block_has_tags, tags = list("keywords"))
+
+  ### Data documentation
+  ###
+  res_data_format <- purrr::map(res_data, roxygen2::block_has_tags, tags = list("format"))
+  res_data_title <- purrr::map(res_data, roxygen2::block_has_tags, tags = list("title"))
+  res_data_description <- purrr::map(res_data, roxygen2::block_has_tags, tags = list("description"))
+
+
+  res_topic <- lapply(res_functions, function(x) {
     topic <- x[["object"]][["topic"]]
-    if (is.null(topic)) {""} else {topic}
+    if (is.null(topic)) {
+      ""
+    } else {
+      topic
+    }
   })
-  res_find_class <- lapply(blocks, function(x) {
-    classe <- class(x[["object"]])[1]
-    if (is.null(classe)) {""} else {classe}
-  })
+
   res_find_filename <- lapply(blocks, function(x) basename(x[["file"]]))
 
   res_find_export <- lapply(blocks, roxygen2::block_has_tags, tags = list("export"))
@@ -75,11 +133,19 @@ find_missing_tags <- function (package.dir = ".", roclets = NULL, load_code = NU
 
   res_find_rdname_value <- lapply(blocks, function(x) {
     rdname <- roxygen2::block_get_tag_value(x, tag = "rdname")
-    if (is.null(rdname)) {""} else {rdname}
+    if (is.null(rdname)) {
+      ""
+    } else {
+      rdname
+    }
   })
   res_find_return_value <- lapply(blocks, function(x) {
     return <- roxygen2::block_get_tag_value(x, tag = "return")
-    if (is.null(return)) {""} else {return}
+    if (is.null(return)) {
+      ""
+    } else {
+      return
+    }
   })
 
   res <- tibble(
@@ -90,7 +156,7 @@ find_missing_tags <- function (package.dir = ".", roclets = NULL, load_code = NU
     return_value = unlist(res_find_return_value),
     has_nord = unlist(res_find_nord),
     rdname_value = unlist(res_find_rdname_value)
-    ) %>%
+  ) %>%
     mutate(
       rdname_value = if_else(rdname_value == "", topic, rdname_value),
       id = 1:n()
@@ -115,22 +181,24 @@ find_missing_tags <- function (package.dir = ".", roclets = NULL, load_code = NU
     mutate(
       not_empty_return_value = (return_value != ""),
       test_has_export_and_return = ifelse((has_export & has_return & not_empty_return_value) | !has_export,
-                                          "ok", "not_ok"),
+        "ok", "not_ok"
+      ),
       test_has_export_or_has_nord = ifelse((!has_export & has_nord) | has_export, "ok", "not_ok")
-  )
+    )
 
-  res_return_error <- res_join[res_join$test_has_export_and_return == "not_ok",]
+  res_return_error <- res_join[res_join$test_has_export_and_return == "not_ok", ]
   if (nrow(res_return_error) != 0) {
     message("Missing or empty return value for exported functions: ", paste(res_return_error$topic, collapse = ", "), "\n\n")
   }
-  res_export_error <- res_join[res_join$test_has_export_or_has_nord == "not_ok",]
+  res_export_error <- res_join[res_join$test_has_export_or_has_nord == "not_ok", ]
   if (nrow(res_export_error) != 0) {
     message("Doc available but need to choose between `@export` or `@noRd`: ", paste(res_export_error$topic, collapse = ", "), "\n\n")
   }
 
   # Set NAMESPACE back to normal
   roxygen2::roxygenise(package.dir = package.dir)
-  return(res_join)
+  res <- split(res_join, res_join$has_nord)
+  return(res)
   # results <- lapply(roclets, roxygen2:::roclet_process, blocks = blocks,
   #                   env = env, base_path = base_path)
   # out <- purrr::map2(roclets, results, roxygen2:::roclet_output, base_path = base_path,
--- tests/testthat/test-find_missing_values.R
+++ tests/testthat/test-find_missing_values.R
@@ -1,29 +1,104 @@
 path <- suppressWarnings(create_example_pkg())
 
 test_that("find_missing_tags works", {
-
   if (packageVersion("roxygen2") > "7.1.2") {
     expect_warning(
-      out <- expect_message(find_missing_tags(path), "my_long_fun_name_for_multiple_lines_globals"),
-      regexp = "@return")
+      out <- expect_message(
+        find_missing_tags(path),
+        "my_long_fun_name_for_multiple_lines_globals"
+      ),
+      regexp = "@return"
+    )
   } else {
-    out <- expect_message(find_missing_tags(path), "my_long_fun_name_for_multiple_lines_globals")
+    out <- expect_message(
+      find_missing_tags(path),
+      "my_long_fun_name_for_multiple_lines_globals"
+    )
   }
+
   expect_equal(nrow(out), 5)
   expect_equal(out$filename, rep("function.R", 5))
-  expect_equal(out$topic, c("my_long_fun_name_for_multiple_lines_globals", "my_plot",
-                            "my_not_exported_doc", "my_not_exported_nord", "my_plot_rdname"))
+  expect_equal(
+    out$topic,
+    c(
+      "my_long_fun_name_for_multiple_lines_globals", "my_plot",
+      "my_not_exported_doc", "my_not_exported_nord", "my_plot_rdname"
+    )
+  )
   expect_false("my_not_exported_nodoc" %in% out$topic)
   expect_equal(out$has_export, c(TRUE, TRUE, FALSE, FALSE, TRUE))
   # last is true because of rdname tag
+
   if (packageVersion("roxygen2") > "7.1.2") {
-    expect_equal(out$has_return, c(FALSE, TRUE, FALSE, FALSE, TRUE))
+    expect_equal(
+      out$has_return,
+      c(FALSE, TRUE, FALSE, FALSE, TRUE)
+    )
   } else {
-    expect_equal(out$has_return, c(TRUE, TRUE, FALSE, FALSE, TRUE))
+    expect_equal(
+      out$has_return,
+      c(TRUE, TRUE, FALSE, FALSE, TRUE)
+    )
   }
-  expect_equal(out$has_nord, c(FALSE, FALSE, FALSE, TRUE, FALSE))
-  expect_equal(out$test_has_export_and_return, c("not_ok", "ok", "ok", "ok", "ok"))
-  expect_equal(out$test_has_export_or_has_nord, c("ok", "ok", "not_ok", "ok", "ok"))
+  expect_equal(
+    out$has_nord,
+    c(FALSE, FALSE, FALSE, TRUE, FALSE)
+  )
+  expect_equal(
+    out$test_has_export_and_return,
+    c("not_ok", "ok", "ok", "ok", "ok")
+  )
+  expect_equal(
+    out$test_has_export_or_has_nord,
+    c("ok", "ok", "not_ok", "ok", "ok")
+  )
+
+  withr::with_dir(path, {
+    usethis::use_package_doc()
+    usethis::use_pipe()
+    usethis::use_data(iris)
+    use_data_doc("iris")
+
+    expect_warning(
+      attachment::att_amend_desc(),
+      regexp = "@return"
+    )
+  })
+
+  if (packageVersion("roxygen2") > "7.1.2") {
+    expect_warning(
+      out <- expect_message(
+        find_missing_tags(path),
+        "my_long_fun_name_for_multiple_lines_globals"
+      ),
+      regexp = "@return"
+    )
+  } else {
+    out <- expect_message(
+      find_missing_tags(path),
+      "my_long_fun_name_for_multiple_lines_globals"
+    )
+  }
+
+  false_positive <- out %>%
+    dplyr::filter(
+      filename %in% c("checkpackage-package.R", "doc_iris.R", "utils-pipe.R")
+    )
+
+  expect_equal(
+    false_positive$has_nord,
+    c(FALSE, FALSE, FALSE)
+  )
+
+  expect_equal(
+    false_positive$test_has_export_and_return,
+    c("ok", "ok", "ok")
+  )
+
+  expect_equal(
+    false_positive$test_has_export_or_has_nord,
+    c("not_ok", "not_ok", "ok")
+  )
 })
 
 # Remove path
